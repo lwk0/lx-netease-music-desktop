073aa508d1a0b2183089ca1261e63753ad417c2c
lwk0 <lwk@users.noreply.github.com>
2026-08-09 01:25:42 +0800

    Revert "fix(kg): 酷狗搜索封面使用 https URL，并修复封面加载失败时的响应式降级"
    This reverts commit f9e4ebcb52f64c000ce84b434cb90c2dd0216249.

--- src/renderer/utils/musicSdk/kg/musicSearch.js
+++ src/renderer/utils/musicSdk/kg/musicSearch.js
@@ -56,7 +56,7 @@ export default {
       source: 'kg',
       interval: formatPlayTime(rawData.Duration),
       _interval: rawData.Duration,
-      img: rawData.Image ? rawData.Image.replace('{size}', '480').replace(/^http:/, 'https:') : null,
+      img: rawData.Image ? rawData.Image.replace('{size}', '480') : null,
       lrc: null,
       otherSource: null,
       hash: rawData.FileHash,
